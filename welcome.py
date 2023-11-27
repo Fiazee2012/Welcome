a88110c333e3f7e4a50dbe00b6ee849be7aa8f02
Fiazee2012 <burhanfaizullah@gmail.com>
2023-11-27 19:02:28 +0300

    INPUT!!!!!!!

--- welcome.py
+++ welcome.py
@@ -3,3 +3,11 @@ institute = "Kuazone"
 number = 9
 print("Hi my name is, " + name + " I am a sudent at " + institute)
 print(type(number))
+
+print("Welcome to Visual Studio Code, in short VsCode. To get started please ")
+name = input("enter your name: ")
+print("and... ")
+country = input("enter your country name:")
+print("Last but not least...")
+email = input("enter your email:")
+print("So I see your name is " + name + ", you are from " + country + " and you have entered your email which is" + email +". Good, now we can get started!")
