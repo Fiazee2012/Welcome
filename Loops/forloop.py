77ada976f6ea0e9b514d2da5657cefeaed1434d0
Fiazee2012 <burhanfaizullah@gmail.com>
2024-01-10 19:18:04 +0300

    Loops

---
+++ Loops/forloop.py
@@ -0,0 +1,11 @@
+Languages = ["HTML", "CSS", "Bootsrap", "Javascript","Github", "Gitbash", "Codepen", "Replit", "Scratch"]
+print(Languages)
+
+for language in Languages:
+    print(language)
+
+Scores = [78, 56, 58, 87, 66, 83, 62, 54]
+print(Scores)
+
+for score in Scores:
+    print(score)
