907b073bbc522fe659efba822e6e89a6cf3d9422
Fiazee2012 <burhanfaizullah@gmail.com>
2024-01-22 18:08:30 +0300

    while loops

---
+++ Loops/whileloop.py
@@ -0,0 +1,16 @@
+i = 1
+
+while i <= 100:
+    print("Burhan is king!!!!", i)
+    i += 1
+
+n = int(input("Enter a number: "))
+while n != 0:
+    n = int(input("Enter a 2 digit number with zero: "))
+    print(n)
+
+j = int(input("Enter any number: "))
+if (j % 2 == 0):
+    print("Your number is even!")
+else:
+    print("It ain't even!")
